4616552bec7f5504ca72a2562757f3f69557bf80
Igor Gladkiy <egunka1988@gmail.com>
2022-02-15 15:09:22 +0200

    add local storage functional

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,17 @@ import { ContactForm } from './ContactForm/ContactForm';
 
 export class App extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const data = localStorage.getItem('contacts');
+    const parsedData = JSON.parse(data);
+    if (parsedData) {
+      this.setState({ contacts: parsedData });
+    }
+  }
 
   addContact = contact => {
     this.setState(prevState => ({
@@ -38,6 +41,12 @@ export class App extends React.Component {
       contacts: this.state.contacts.filter(item => item.id !== elemToRemove),
     });
   };
+
+  componentDidUpdate(prevState, prevProps) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     return (
